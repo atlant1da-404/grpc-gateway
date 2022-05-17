7257104931e637cb505c226cb5dae36a66781652
atlant1da-404 <ilyshawenn@protonmail.com>
2022-05-17 11:42:10 +0300

    configs refactor

--- server/server.go
+++ server/server.go
@@ -1,11 +1,10 @@
-package main
+package server
 
 import (
 	"context"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
-	"grpc-gateway/config"
-	"grpc-gateway/internal/app"
+	"grpc-gateway/internal/handler"
 	"grpc-gateway/pkg/gen/notes"
 	"log"
 	"net"
@@ -16,26 +15,39 @@ const (
 	method = "tcp"
 )
 
-func main() {
+type Server struct {
+	socketOne string
+	socketTwo string
+}
 
-	hosts := config.LoadSocketConfig()
+func NewServer(socketOne, socketTwo string) *Server {
+
+	return &Server{
+		socketOne: socketOne,
+		socketTwo: socketTwo,
+	}
+}
+
+func (s Server) RunAPIServer() {
 
 	go func() {
 
 		mux := runtime.NewServeMux()
-		notes.RegisterNotesHandlerServer(context.Background(), mux, app.NewNotes())
-		log.Fatalln(http.ListenAndServe(hosts[0], mux))
+		notes.RegisterNotesHandlerServer(context.Background(), mux, handler.NewNotes())
+		log.Fatalln(http.ListenAndServe(s.socketOne, mux))
 	}()
 
-	listener, err := net.Listen(method, hosts[1])
+	listener, err := net.Listen(method, s.socketTwo)
 	if err != nil {
 		log.Fatalln(err.Error())
+		return
 	}
 
 	grpcServer := grpc.NewServer()
-	notes.RegisterNotesServer(grpcServer, app.NewNotes())
+	notes.RegisterNotesServer(grpcServer, handler.NewNotes())
 	err = grpcServer.Serve(listener)
 	if err != nil {
 		log.Println(err.Error())
+		return
 	}
 }
